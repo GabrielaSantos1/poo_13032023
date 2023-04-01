aefd5b994c0ba43b9ce2a7a1ca0d66fa36c7df01
GabrielaSantos1 <104846859+GabrielaSantos1@users.noreply.github.com>
2023-04-01 19:16:57 -0300

    Update dowhile.cpp

--- dowhile.cpp
+++ dowhile.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int main() {
-
+    
+    //cria uma variavel inteira 
     int num;
     
+    //faz oq tiver dentro
     do{
+        //mostra pro user 
         cout << "Digite um número de 1 à 10: ";
+        //coloca a resposta como valor da variavel num
         cin >> num;
+    //enquanto o valor de num for menor que 1 e maior que 10
     } while (num <1 || num > 10);
     
+    //quando não for, mostra pro user o valor digitado
     cout << "Você digitou o número " << num << ".";
     return 0;
 }
