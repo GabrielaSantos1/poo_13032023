52755a08e37e81b8f16c0209e6afbac8d2ca2b1d
GabrielaSantos1 <104846859+GabrielaSantos1@users.noreply.github.com>
2023-04-03 19:42:58 -0300

    Update calculadora_infinita.cpp

--- calculadora_infinita.cpp
+++ calculadora_infinita.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-float a, b;
+int main() {
 
-float Soma() {
-    float Resultado;
-    Resultado = a + b;
-    return Resultado;}
+char continuar;
     
-float Subt() {
-    float Resultado;
-    Resultado = a - b;
-    return Resultado;}
+do{
+    double ac1, ac2, af;
     
-float Mult() {
-    float Resultado;
-    Resultado = a * b;
-    return Resultado;}
+    cout << "Digite a nota da sua AC1 e dê ENTER: ";
+    cin >> ac1;
     
-float Divi() {
-    float Resultado;
-    Resultado = a / b;
-    return Resultado;}
+    cout << "Digite a nota da sua AC2 e dê ENTER: ";
+    cin >> ac2;
     
-    int main(){
-
-    cout << "Digite dois números positivos: " << endl;
-    cin >> a >> b;
+    cout << "Digite a nota da sua AF e dê ENTER: ";
+    cin >> af;
     
-    cout << "Soma = " << Soma() << endl;
-    cout << "Subtração = " <<  Subt() << endl;
-    cout << "Multiplicação = " <<  Mult() << endl;
-    cout << "Divisão = " <<  Divi() << endl;
+    ac1 = ac1 * 0.15;
     
-
+    ac2 = ac2 * 0.35;
+    
+    af = af * 0.5;
+    
+   
+    double soma = (ac1 + ac2 + af);
+    
+    cout << "A sua média no semestre é " << soma << endl;
+    cout << "AC1 = " << ac1 << "; AC2 = " << ac2 << "; AF = " << af << ";" << endl;
+    
+    if (soma >=5){
+        cout << "Parabéns! Você foi aprovado!" << endl;
+    }else{
+        cout << "Eita! Você foi reprovado :c" << endl;
+    }
+    cout << "Deseja continuar? s/n: ";
+    cin >> continuar;
+    
+}while(continuar=='s' || continuar=='S');
+    
+    
+return 0;
+    
 }
